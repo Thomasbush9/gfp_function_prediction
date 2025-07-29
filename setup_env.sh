4770fd401df2bd9a34b267c0b9f0d71722efcda8
thomas bush <thomasbush52@gmail.com>
2025-07-29 00:19:55 -0400

    changed setup script  to abs path

--- setup_env.sh
+++ setup_env.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Run this once per setup
 
+#!/bin/bash
+# Run this once per setup
+
 echo "[+] Creating conda environment for Boltz"
 conda create -y -n boltz-infer python=3.10
+
+# Ensure conda can be activated in non-interactive shell
+source $(conda info --base)/etc/profile.d/conda.sh
 conda activate boltz-infer
 
 echo "[+] Installing requirements"
