56d2fd831423e8eadb25c0dc9085217db89e8291
thomas bush <thomasbush52@gmail.com>
2025-07-31 14:54:47 -0400

    env to mamba

--- bash_scripts/setup_env.sh
+++ bash_scripts/setup_env.sh
@@ -5,11 +5,11 @@
 # Run this once per setup
 
 echo "[+] Creating conda environment for Boltz"
-conda create -y -n boltz-infer python=3.10
+mamba create -y -n boltz-infer python=3.10
 
 # Ensure conda can be activated in non-interactive shell
 source $(conda info --base)/etc/profile.d/conda.sh
-conda activate boltz-infer
+mamba activate boltz-infer
 
 echo "[+] Installing requirements"
 pip install -r requirements.txt  # optional if you use this
