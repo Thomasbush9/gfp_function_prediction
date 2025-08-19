a02bcbb72285ffa13043da2bdd490c967c2e85b0
thomas bush <thomasbush52@gmail.com>
2025-08-19 16:46:04 -0400

    feature extraction confs, coords and mat

--- models/modules/feature_extraction.py
+++ models/modules/feature_extraction.py
@@ -2,10 +2,12 @@ import os
 from argparse import ArgumentParser
 from pathlib import Path
 
+import numpy as np
 import torch
 from tqdm import tqdm
 
 from .protein import Protein
+from .utils import list2onehot
 
 """
 It will expect a dir path structure like this:
@@ -22,16 +24,19 @@ it will generate a data loader with the files:
 
 
 def extract_data_from_dir(dir: Path):
-    dir_name = dir.name
-    cif_file_path = (
-        dir
-        / f"boltz_results_{dir.name}"
-        / "predictions"
-        / dir.name
-        / f"{dir.name}_model_0.cif"
-    )
+    """
+    Args:
+    - dir: Path object, path to a single directory of predictions
+
+    Returns:
+    - cif file,
+    - confidences: Tensor, (num_res, )
+    """
+    dir_pred = dir / f"boltz_results_{dir.name}" / "predictions" / dir.name
+    cif_file_path = dir_pred / f"{dir.name}_model_0.cif"
+    confs = np.load(dir_pred / f"plddt_{dir.name}_model_0.npz")
     cif_file = Protein(cif_file_path)
-    return cif_file
+    return cif_file, torch.Tensor(confs["plddt"])
 
 
 if __name__ == "__main__":
@@ -43,6 +48,7 @@ if __name__ == "__main__":
     dir_path = Path(args.dir)
 
     for i in dir_path.iterdir():
-        cif = extract_data_from_dir(i)
-        print(cif.neigh_idx)
-        break
+        cif, confs = extract_data_from_dir(i)
+        adj_mat = list2onehot(cif.neigh_idx, 238)
+        coords = torch.Tensor(cif.coord)
+        feat = torch.cat((coords, confs.unsqueeze(dim=-1)), dim=1)
