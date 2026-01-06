c8437dbe2f9250726729d440926689fbe9148416
thomas bush <thomasbush52@gmail.com>
2026-01-06 13:45:41 -0500

    added save opt

--- utils/generate_random_data.py
+++ utils/generate_random_data.py
@@ -1,7 +1,6 @@
 from .utils import generate_mutation_dataset, load_seq_
-from pathlib import Path
-import yaml
 from argparse import ArgumentParser
+from pathlib import Path
 
 
 
@@ -9,13 +8,14 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--seq", type=str)
     parser.add_argument("--n", type=int)
+    parser.add_argument("--out_path", type=str)
 
 
     args = parser.parse_args()
     seq, _ = load_seq_(args.seq, fasta=False)
-    print(seq)
     df = generate_mutation_dataset(seq, args.n)
-    print(df.head(4))
+    df.to_csv(Path(args.out_path)/"GARBRB3.csv")
+    
 
 
 
